431078c1fdae38ea2cc054459146e6d97f097ee4
mango41 <zlh3108452524@gmail.com>
2023-05-27 21:00:06 +0800

    feat:教师侧边栏

--- code/learning system/vue/src/components/StudentAside.vue
+++ code/learning system/vue/src/components/StudentAside.vue
@@ -11,7 +11,7 @@
     <div style="height: 60px; line-height: 60px; text-align: center">
       <img src="@/assets/logo.png" alt="" style="width: 20px; position:relative; top:5px; margin-right: 5px; ">
       <!--这里不能用logoTextShow 原因未知-->
-      <b style="color: white" v-show="!isCollapse">学生考试系统</b>
+      <b style="color: white" v-show="!isCollapse">在线学习系统</b>
     </div>
     <el-submenu index="1-1">
       <template slot="title">
@@ -22,14 +22,6 @@
         <i class="el-icon-house"></i>
         <span slot="title">回到主页</span>
       </el-menu-item>
-      <el-menu-item index="/student/my-exam">
-        <i class="el-icon-notebook-2"></i>
-        <span slot="title">我要考试</span>
-      </el-menu-item>
-      <el-menu-item index="/student/my-score">
-        <i class="el-icon-notebook-2"></i>
-        <span slot="title">我的成绩</span>
-      </el-menu-item>
       <el-menu-item index="/student/self">
         <i class="el-icon-edit"></i>
         <span slot="title">个人信息</span>
@@ -39,8 +31,29 @@
         <span slot="title">退出登录</span>
       </el-menu-item>
     </el-submenu>
+
+    <el-submenu index="1-2">
+      <template slot="title">
+        <i class="el-icon-search"></i>
+        <span slot="title">系统功能</span>
+      </template>
+      <el-menu-item index="/student/my-score">
+        <i class="el-icon-notebook-2"></i>
+        <span slot="title">我的成绩</span>
+      </el-menu-item>
+      <el-menu-item index="/student/my-exam">
+        <i class="el-icon-notebook-2"></i>
+        <span slot="title">我的考试</span>
+      </el-menu-item>
+    </el-submenu>
+
   </el-menu>
 </template>
+
+
+
+
+
 
 
 <script>
--- code/learning system/vue/src/components/TeacherAside.vue
+++ code/learning system/vue/src/components/TeacherAside.vue
@@ -1,11 +1,79 @@
 <template>
+  <el-menu :default-openeds="['1', '3']" style="min-height: 935px; overflow-x: hidden"
+           background-color="rgb(48,65,86)"
+           text-color="#fff"
+           active-text-color="#ffd04b"
+           :collapse-transition="false"
+           :collapse="isCollapse"
+           router
+           @select="handleSelect"
+  >
+    <div style="height: 60px; line-height: 60px; text-align: center">
+      <img src="@/assets/logo.png" alt="" style="width: 20px; position:relative; top:5px; margin-right: 5px; ">
+      <!--这里不能用logoTextShow 原因未知-->
+      <b style="color: white" v-show="!isCollapse">在线学习系统</b>
+    </div>
+    <el-submenu index="1-1">
+      <template slot="title">
+        <i class="el-icon-more-outline"></i>
+        <span slot="title">个人功能</span>
+      </template>
+      <el-menu-item index="/teacher/home">
+        <i class="el-icon-house"></i>
+        <span slot="title">回到主页</span>
+      </el-menu-item>
+      <el-menu-item index="/teacher/self">
+        <i class="el-icon-edit"></i>
+        <span slot="title">个人信息</span>
+      </el-menu-item>
+      <el-menu-item index="/login">
+        <i class="el-icon-switch-button"></i>
+        <span slot="title">退出登录</span>
+      </el-menu-item>
+    </el-submenu>
 
+    <el-submenu index="1-2">
+      <template slot="title">
+        <i class="el-icon-search"></i>
+        <span slot="title">系统功能</span>
+      </template>
+      <el-menu-item index="/teacher/question-control">
+        <i class="el-icon-notebook-1"></i>
+        <span slot="title">题目管理</span>
+      </el-menu-item>
+      <el-menu-item index="/teacher/question-property">
+        <i class="el-icon-notebook-1"></i>
+        <span slot="title">题目性质</span>
+      </el-menu-item>
+      <el-menu-item index="/teacher/auto-compose">
+        <i class="el-icon-s-check"></i>
+        <span slot="title">自动组卷</span>
+      </el-menu-item>
+      <el-menu-item index="/teacher/manual-compose">
+        <i class="el-icon-folder-opened"></i>
+        <span slot="title">手动组卷</span>
+      </el-menu-item>
+    </el-submenu>
+
+  </el-menu>
 </template>
 
+
 <script>
+
 export default {
-  name: "TeacherAside"
+  name: "TeacherAside",
+  props: {
+    isCollapse: Boolean,
+    logoTextShow: Boolean
+  },
+  methods: {
+    handleSelect(index) {
+      console.log(this.$router.options.routes)
+    }
+  }
 }
+
 </script>
 
 <style scoped>
