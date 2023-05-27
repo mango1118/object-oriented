6b406c83edb8f4c324756b7fcfdba0739dfa4a47
mango41 <zlh3108452524@gmail.com>
2023-05-27 19:57:42 +0800

    delete:删除vue

--- code/learning system/src/main/java/com/oo/LearningSystemApplication.java
+++ code/learning system/src/main/java/com/oo/LearningSystemApplication.java
@@ -8,6 +8,7 @@ package com.oo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LearningSystemApplication extends SpringBootServletInitializer {
 
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(LearningSystemApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearningSystemApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LearningSystemApplication.class, args);
