0375d40d91dbd3cfae26696bb710c68e32bb20be
mango41 <zlh3108452524@gmail.com>
2023-05-27 20:32:20 +0800

    feat:登陆页面

--- code/learning system/vue/src/main.js
+++ code/learning system/vue/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/gloable.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {size: "small"})
+
+
+import axios from "./utils/request"
+Vue.prototype.axios = axios;
+
+localStorage.setItem("user", null)
+
+// 路由守卫
+// const WhiteListRouter = ['/login']
+// router.beforeEach((to, from, next) => {
+//     let user = store.
+// })
+
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- code/learning system/vue/src/views/others/Login.vue
+++ code/learning system/vue/src/views/others/Login.vue
@@ -1,13 +1,72 @@
 <template>
-
+  <div class="wrapper">
+    <div
+        style="margin: 200px auto; background-color: #fff; width: 350px; height: 300px; padding: 20px; border-radius: 10px">
+      <div style="margin: 20px 0; text-align: center; font-size: 24px"><b>登 录</b></div>
+      <el-form :model="user" :rules="rules" ref="userForm">
+        <el-form-item prop="username">
+          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-user" v-model="user.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password
+                    v-model="user.password"></el-input>
+        </el-form-item>
+        <el-form-item style="margin: 10px 0; text-align: right">
+          <el-button type="primary" size="small" autocomplete="off" @click="login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
+// import{setRoutes} from "@/router"
+
 export default {
-  name: "Login"
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "login",
+  data() {
+    return {
+      user: {},
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+        ],
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs['userForm'].validate((valid) => {
+        if (valid) {//表单合法
+          this.axios.post("http://localhost:9090/login", this.user).then(res => {
+            console.log(res);
+            if (res.code === 10001 || res.code === 10002) {
+              localStorage.setItem("user", JSON.stringify(res.data))
+              this.$message.success("登录成功");
+              if (res.code === 10001) {//学生
+                this.$router.push("/reader");
+              } else if (res.code === 10002) {//老师
+                this.$router.push("/admin");
+              }
+            } else {
+              this.$message.error(res.msg);
+            }
+          })
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+.wrapper {
+  height: 100vh;
+  background-image: linear-gradient(to bottom right, #FC466B, #3F5EFB);
+  overflow: hidden;
+}
 </style>
