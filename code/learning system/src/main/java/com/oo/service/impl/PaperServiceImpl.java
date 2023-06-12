ac1090bdf5ede53731ed45177d70b4a860715937
xxx <1363993807@qq.com>
2023-06-12 21:11:29 +0800

    查卷逻辑

--- code/learning system/src/main/java/com/oo/service/impl/PaperServiceImpl.java
+++ code/learning system/src/main/java/com/oo/service/impl/PaperServiceImpl.java
@@ -1,7 +1,20 @@
 package com.oo.service.impl;
-
+import com.oo.dao.PaperDao;
+import com.oo.dao.PaperQuestionDao;
+import com.oo.dao.QuestionDao;
 import com.oo.service.PaperService;
+import com.oo.domain.Paper;
+import com.oo.domain.PaperQuestion;
+import com.oo.domain.Question;
+import com.oo.domain.vo.Qcontent_score;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @description:
@@ -10,15 +23,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PaperServiceImpl implements PaperService {
+
     @Autowired
     private PaperDao paperDao;
     @Autowired
-    private ParperQuestionDao paperquestionDao;
+    private PaperQuestionDao paperquestionDao;
     @Autowired
     private QuestionDao questionDao;
 
     @Override
     public Paper selectById(Integer id) {
+        //System.out.println(paperDao.selectById(id));
         return paperDao.selectById(id);
     }
 
@@ -29,13 +44,16 @@ public class PaperServiceImpl implements PaperService {
         QueryWrapper<PaperQuestion> queryWrapper1 = new QueryWrapper<>();
         queryWrapper.select("question_id").eq("paper_id",id);
         queryWrapper1.select("question_id", "set_score").eq("paper_id",id);
-        List<Map<String, Object>> questionIds = paperquestionDao.selectList(queryWrapper);
-        List<Map<String, Object>> questionIdScores = paperquestionDao.selectList(queryWrapper1);
+        List<Map<String, Object>> map = paperquestionDao.selectMaps(queryWrapper);
+        List<Map<String, Object>> questionIdScores = paperquestionDao.selectMaps(queryWrapper1);
+        List<Object> questionIds =  map.stream()
+                .map(m -> m.get("question_id"))
+                .collect(Collectors.toList());
 
         // 根据question_id列表查询question表content
         QueryWrapper<Question> queryWrapper2 = new QueryWrapper<>();
         queryWrapper2.select("id", "content").in("id", questionIds);
-        List<Map<String, Object> questionContents = questionDao.selectList(queryWrapper2);
+        List<Map<String, Object>> questionContents = questionDao.selectMaps(queryWrapper2);
 
         // 链接两个列表，封装到Qcontent_score中
         List<Qcontent_score> result = new ArrayList<>();
@@ -44,16 +62,16 @@ public class PaperServiceImpl implements PaperService {
             Integer score = (Integer)qIdScore.get("set_score");
 
             for (Map<String, Object> question : questionContents) {
+
                 if (question.get("id").equals(questionId)) {
                     Qcontent_score qcScore = new Qcontent_score();
-                    qcScore.setScore(score);
+                    qcScore.setSetScore(score);
                     qcScore.setContent((String)question.get("content"));
                     result.add(qcScore);
                     break;
                 }
             }
         }
-
         return result;
 
     }
