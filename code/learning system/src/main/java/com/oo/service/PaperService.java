3cf1a1817bcc076f78972d071c788a0e531e439b
xxx <1363993807@qq.com>
2023-06-12 14:08:29 +0800

    查看试卷功能

--- code/learning system/src/main/java/com/oo/service/PaperService.java
+++ code/learning system/src/main/java/com/oo/service/PaperService.java
@@ -9,4 +9,20 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Transactional
 public interface PaperService {
+    /**
+     * 通过id返回paper (name,totalscore)
+     * 即查询试卷名字，总分
+     * @param id
+     * @return paper
+     */
+    public Parper selectById(Integer id);
+
+    /**
+     * 通过paper_id返回paper(question_content,score)
+     * 即查询试卷每道题干，分值
+     * @param id
+     * @return List<Qcontent_score>
+     */
+    public List<Qcontent_score> selectByPaperId(Integer id);
+
 }
