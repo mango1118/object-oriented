e7ddf2e5e8c08b2eddba396e815f12edd0feb15a
xxx <1363993807@qq.com>
2023-06-12 21:10:49 +0800

    查卷逻辑

--- code/learning system/src/main/java/com/oo/service/PaperService.java
+++ code/learning system/src/main/java/com/oo/service/PaperService.java
@@ -1,6 +1,10 @@
 package com.oo.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import com.oo.domain.Paper;
+import com.oo.domain.vo.Qcontent_score;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description:
@@ -9,13 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Transactional
 public interface PaperService {
+
     /**
      * 通过id返回paper (name,totalscore)
      * 即查询试卷名字，总分
      * @param id
      * @return paper
      */
-    public Parper selectById(Integer id);
+    public Paper selectById(Integer id);
 
     /**
      * 通过paper_id返回paper(question_content,score)
