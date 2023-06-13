355109b1bb314ef27fea3195725f299c3804a46c
mango41 <zlh3108452524@gmail.com>
2023-06-13 16:36:23 +0800

    feat:教师批改页面

--- code/learning system/vue/src/views/controller/teacher/CorrectQuestionView.vue
+++ code/learning system/vue/src/views/controller/teacher/CorrectQuestionView.vue
@@ -1,13 +1,141 @@
 <template>
+  <!--
+    json
+    {
+      "msg":"msg1",
+      "code":"123",
+      "data":[
+        "data":[
+          {
+            "id":"1",
+            "content":"题目1内容",
+            "answerImage":"url1",
+            "score":10,
+            "teacherScore":0,
+            "submitted":false
+          },
+          {
+            "id":"2",
+            "content":"题目1内容",
+            "answerImage":"url2",
+            "score":15,
+            "teacherScore":0,
+            "submitted":true
+          },
+        ]
+      ]
+    }
+  -->
 
+
+  <div>
+    <h2>批改题目</h2>
+    <el-card v-for="(question, index) in questions" :key="index" class="question-card">
+      <div class="question-content">{{ question.content }}</div>
+      <div class="student-answer">
+        <img :src="question.answerImage" alt="学生作答">
+      </div>
+      <div class="score-row">
+        <div class="question-score">分值: {{ question.score }}</div>
+        <div class="teacher-score">
+          <el-form :model="question" ref="scoreForm" label-width="60px">
+            <el-form-item label="教师给分" label-width="20">
+              <el-input-number v-model="question.teacherScore" :min="0" :max="question.score"
+                               :disabled="question.submitted"></el-input-number>
+            </el-form-item>
+          </el-form>
+        </div>
+        <el-button class="confirm-button" type="primary" @click="confirmScore(index)" :disabled="question.submitted">
+          确认
+        </el-button>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "CorrectQuestionView"
-}
+  data() {
+    return {
+      questions: []
+    };
+  },
+  mounted() {
+    // 从后端获取题目数据的逻辑
+    this.fetchQuestions();
+  },
+  methods: {
+    fetchQuestions() {
+      axios
+          .get('/api/questions') // 通过合适的URL获取题目数据
+          .then(response => {
+            this.questions = response.data;
+          })
+          .catch(error => {
+            console.error('Failed to fetch questions:', error);
+          });
+    },
+    confirmScore(index) {
+      const question = this.questions[index];
+      question.submitted = true;
+
+      // 构建需要发送的表单数据
+      const formData = {
+        questionId: question.id,
+        teacherScore: question.teacherScore
+      };
+
+      // 使用axios或其他HTTP客户端库发送表单数据到后端
+      axios
+          .post('/api/submit-score', formData) // 根据实际情况修改URL和发送方法
+          .then(response => {
+            // 处理成功发送后的逻辑
+            console.log('Score submitted successfully!');
+            this.questions.splice(index, 1);
+          })
+          .catch(error => {
+            // 处理发送失败的逻辑
+            console.error('Failed to submit score:', error);
+          });
+    }
+  }
+};
 </script>
 
 <style scoped>
+.question-card {
+  margin-bottom: 20px;
+  padding: 20px;
+}
 
+.question-content {
+  font-weight: bold;
+}
+
+.student-answer img {
+  max-width: 200px;
+  max-height: 200px;
+  margin-top: 10px;
+}
+
+/*.score-row {*/
+/*  margin-top: 10px;*/
+/*  display: flex;*/
+/*  align-items: center;*/
+/*}*/
+
+.question-score {
+  margin-top: 10px;
+}
+
+.teacher-score {
+  margin-top: 10px;
+}
+
+.confirm-button {
+  margin-top: 20px;
+  margin-left: 300px;
+}
 </style>
