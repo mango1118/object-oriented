0a1cbfe502ddae288212cba2d414ba0629a2a530
xxx <1363993807@qq.com>
2023-06-14 23:22:45 +0800

    Delete all

--- code/learning system/src/main/java/com/oo/service/PaperService.java
+++ code/learning system/src/main/java/com/oo/service/PaperService.java
@@ -1,10 +1,7 @@
 package com.oo.service;
 
 import org.springframework.transaction.annotation.Transactional;
-import com.oo.domain.Paper;
-import com.oo.domain.vo.Qcontent_score;
-import java.util.List;
-import java.util.Map;
+
 
 /**
  * @description:
@@ -14,20 +11,5 @@ import java.util.Map;
 @Transactional
 public interface PaperService {
 
-    /**
-     * 通过id返回paper (name,totalscore)
-     * 即查询试卷名字，总分
-     * @param id
-     * @return paper
-     */
-    public Paper selectById(Integer id);
-
-    /**
-     * 通过paper_id返回paper(question_content,score)
-     * 即查询试卷每道题干，分值
-     * @param id
-     * @return List<Qcontent_score>
-     */
-    public List<Qcontent_score> selectByPaperId(Integer id);
 
 }
