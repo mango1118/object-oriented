ac185e2f3d491f726a91a5d240e6f53f784ab79b
moqiuxia <2582459702@qq.com>
2023-06-18 15:29:48 +0800

    手动组卷:创建试卷

--- code/learning system/src/main/java/com/oo/controller/QuestionController.java
+++ code/learning system/src/main/java/com/oo/controller/QuestionController.java
@@ -1,7 +1,17 @@
 package com.oo.controller;
 
+import com.oo.controller.R.Code;
+import com.oo.controller.R.Result;
+import com.oo.domain.Question;
+import com.oo.domain.Student;
+import com.oo.service.QuestionService;
+import com.oo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @description:
@@ -11,4 +21,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
+
 }
