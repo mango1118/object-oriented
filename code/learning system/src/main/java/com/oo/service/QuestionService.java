7408b7b5be080ef8767891d1bcc9df9edeefeb79
Siqi33 <3030410478@qq.com>
2023-06-26 15:19:16 +0800

    未知原因丢失增加主观题逻辑恢复

--- code/learning system/src/main/java/com/oo/service/QuestionService.java
+++ code/learning system/src/main/java/com/oo/service/QuestionService.java
@@ -3,7 +3,9 @@ package com.oo.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.oo.domain.Question;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +20,9 @@ public interface QuestionService {
 
 
     public boolean deleteQuestion(Integer id);
+
+    public String upload(MultipartFile graph, String fileUploadPath) throws IOException;
+
 
     public Question getQuestionById(Integer questionId);
 }
--- code/learning system/src/main/java/com/oo/service/impl/QuestionServiceImpl.java
+++ code/learning system/src/main/java/com/oo/service/impl/QuestionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.oo.service.impl;
 
+import cn.hutool.core.io.FileUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -10,9 +11,16 @@ import com.oo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.oo.domain.Question;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+
 
 /**
  * @description:
@@ -32,9 +40,10 @@ public class QuestionServiceImpl implements QuestionService {
     public boolean addQuestion(Question question){
 
         /**
-         * @description: 设性质未有增加（0），题目不可以用于组卷
+         * @description: 设性质未有增加（0），题目不可以用于组卷;初始化deleted
          */
         question.setEnabled(0);
+        question.setDeleted(0);
         return questionDao.insert(question) > 0;
     }
 
@@ -54,6 +63,36 @@ public class QuestionServiceImpl implements QuestionService {
         question.setDeleted(1);
         //System.out.println(question);
         return questionDao.updateById(question) > 0;
+    }
+
+    /**
+     * @description: 将图片转为url保存
+     */
+    public String upload(MultipartFile graph, String fileUploadPath) throws IOException {
+        //拼接磁盘路径
+        File saveFile = new File(fileUploadPath);
+        String savePath = saveFile.getAbsolutePath() + "\\";
+
+        //获取上传文件信息
+        String originalFilename = graph.getOriginalFilename();
+        String type = FileUtil.extName(originalFilename);
+        long size = graph.getSize();
+
+        //找到磁盘目录
+        File uploadParentFile = new File(savePath);
+        // 判断配置的文件目录是否存在，若不存在则创建一个新的文件目录
+        if (!uploadParentFile.exists()) {
+            uploadParentFile.mkdirs();
+        }
+
+        String uuid = IdUtil.fastSimpleUUID();
+        String fileUuid = uuid + StrUtil.DOT + type;
+        //存储到磁盘
+        File uploadFile = new File(savePath + fileUuid);
+        graph.transferTo(uploadFile);
+        //存储到数据库
+        String url = "http://localhost:9090/graph/" + fileUuid;
+        return url;
     }
 
     /**
